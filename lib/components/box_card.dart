04782722b11b18df0dca51fd923b605fe3689d72
Marco.Gomes <Marco.Gomes@sicoob.com.br>
2024-08-06 08:35:39 -0300

    first commit

---
+++ lib/components/box_card.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+
+class BoxCard extends StatelessWidget {
+  final Widget boxContent;
+  const BoxCard({super.key, required this.boxContent});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: boxContent,
+
+    );
+  }
+}
---
+++ lib/components/sections/header.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class Header extends StatelessWidget {
+  const Header({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration( 
+        gradient: LinearGradient(
+          begin: Alignment.center,
+          end: Alignment.bottomCenter,
+          colors: [
+            Color.fromRGBO(103, 99, 234, 1.0),
+            Color.fromRGBO(155, 105, 254, 1.0),
+            Color.fromRGBO(195, 107, 255, 1.0)
+          ]
+        ),
+        borderRadius: BorderRadius.vertical(bottom: Radius.circular(10)) 
+      ),
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(16.0, 80.0, 16.0, 16.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text.rich(
+                      TextSpan(text: '\$',
+                        style: TextStyle(fontSize: 16),
+                        children: [
+                      TextSpan(text: '1000.00',
+                          style: TextStyle(
+                              fontSize: 28, fontWeight: FontWeight.bold))
+                    ])),
+                Text(
+                  'Balanço dísponível',
+                  style: TextStyle(fontSize: 16),
+                )
+              ],
+            ),
+            Icon(
+              Icons.account_circle,
+              size: 42,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
